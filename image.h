7423ea7855d585bf7174ae65f970c9c49a40814d
Enetheru <nicholas.samuel@gmail.com>
2013-07-04 16:00:00 +0930

    homogenised image functions

--- image.h
+++ image.h
@@ -13,8 +13,10 @@ static Evas_Object *
 image_generate_random(Evas *evas, int w, int h)
 {
    Evas_Object *image;
-   int *pixels, i, value;
-   int mask = 0x000000FF;
+   uint32_t *pixels, mask, value;
+   int i;
+
+   mask = 0x000000FF;
 
    /* create default randomly generated image */
    image = evas_object_image_filled_add(evas);
@@ -37,10 +39,11 @@ image_generate_random(Evas *evas, int w, int h)
 }
 
 static Evas_Object *
-image_generate_color(Evas *evas, int w, int h, int color)
+image_generate_color(Evas *evas, int w, int h, uint32_t color)
 {
    Evas_Object *image;
-   int *pixels, i, value;
+   uint32_t *pixels, value;
+   int i;
 
    /* create default 100x100 randomly generated maze */
    image = evas_object_image_filled_add(evas);
@@ -61,10 +64,11 @@ image_generate_color(Evas *evas, int w, int h, int color)
 }
 
 static void
-image_fill_color(Evas_Object *image, int color)
+image_fill_color(Evas_Object *image, uint32_t color)
 {
    int i, w, h;
-   int *pixels;
+   uint32_t *pixels;
+
    if(!image)return;
    evas_object_image_size_get(image, &w, &h);
    pixels = evas_object_image_data_get(image, EINA_TRUE);
@@ -77,9 +81,11 @@ image_fill_color(Evas_Object *image, int color)
 }
 
 static void
-image_paint_pixel(Evas_Object *image, int x, int y, int color)
+image_paint_pixel(Evas_Object *image, int x, int y, uint32_t color)
 {
-   int w, h, *pixels;
+   int w, h;
+   uint32_t *pixels;
+
    if(!image)return;
    evas_object_image_size_get(image, &w, &h);
    if(!(0 < x < w && 0 < y < h))return;
@@ -94,6 +100,7 @@ image_add_pixel(Evas_Object *image, int x, int y, uint32_t color)
 {
    int w, h;
    uint32_t  *pixels, pixel;
+
    if(!image)return;
    evas_object_image_size_get(image, &w, &h);
    if(!(0 < x < w && 0 < y < h))return;
@@ -103,34 +110,35 @@ image_add_pixel(Evas_Object *image, int x, int y, uint32_t color)
    pixels[x+w*y] = pixel_add(pixel, color);
 
    evas_object_image_data_set(image, pixels);
-   evas_object_image_data_update_add(image, 0,0,w,h);
-  
+   evas_object_image_data_update_add(image, 0,0,w,h);  
 }
 
 static void
-image_paint_path(Evas_Object *image, Pathgen_Path *path, int color)
+image_paint_path(Evas_Object *image, Pathgen_Path *path, uint32_t color)
 {
    while(pathgen_path_walk(path))
       image_add_pixel(image, path->current->x, path->current->y, color);      
 }
 
 static void
-image_paint_node(Evas_Object *image, Pathgen_Node *node, int color)
+image_paint_node(Evas_Object *image, Pathgen_Node *node, uint32_t color)
 {
    if(!image || !node)return;
    image_paint_pixel(image, node->x, node->y, color);
 }
 
 static uint32_t
-image_pixel_value_get(Evas_Object *image, Evas_Coord x, Evas_Coord y, uint32_t mask, unsigned int shift)
+image_pixel_value_get(Evas_Object *image, int x, int y, uint32_t mask, int shift)
 {
-   unsigned int w, h;
+   int w, h;
    uint32_t *pixels;
+
    if(!image)return 0;
    evas_object_image_size_get(image, &w, &h);
    if(!(0 < x < w && 0 < y < h))return 0; 
    pixels = evas_object_image_data_get(image, EINA_FALSE);
    evas_object_image_data_set(image, pixels);
+
    return (pixels[x+w*y] & mask)>>shift;
 }
 
